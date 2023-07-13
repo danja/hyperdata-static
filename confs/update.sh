e8749b36bf94dc7aa205379dd6afa8d4d8bc6dc2
Danny Ayers <danny.ayers@gmail.com>
2023-07-13 12:48:21 +0200

    apache conf

---
+++ confs/update.sh
@@ -0,0 +1,25 @@
+
+
+mv /opt/lampp/etc/extra/httpd-vhosts.conf /opt/lampp/etc/extra/httpd-vhosts.conf_
+cp httpd-vhosts.conf /opt/lampp/etc/extra/httpd-vhosts.conf
+chown daemon:root /opt/lampp/etc/extra/httpd-vhosts.conf
+chmod 755 /opt/lampp/etc/extra/httpd-vhosts.conf 
+
+mv /opt/lampp/etc/httpd.conf /opt/lampp/etc/httpd.conf_
+cp httpd.conf /opt/lampp/etc/httpd.conf
+chown daemon:root /opt/lampp/etc/httpd.conf
+chmod 755 /opt/lampp/etc/httpd.conf
+
+mv /opt/lampp/etc/httpd-subdomains.conf httpd-subdomains.conf_
+cp httpd-subdomains.conf /opt/lampp/etc/httpd-subdomains.conf
+chown daemon:root /opt/lampp/etc/httpd-subdomains.conf
+chmod 755 /opt/lampp/etc/httpd-subdomains.conf
+
+
+# /opt/lampp/etc/extra/httpd-ssl.conf
+# /etc/apache2/ports.conf
+# I HAVE REMOVED REDIRECTS FROM 
+# /opt/lampp/etc/extra/httpd-vhosts.conf
+
+
+# -rwxr-xr-x 1 daemon root
